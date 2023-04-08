470cf35d2883685dd9b0347c47cd01672704cbef
65Juko <thejulianjuko@gmail.com>
2023-04-09 00:41:08 +0200

    initial commit

---
+++ grungus-api/Models/User.cs
@@ -0,0 +1,33 @@
+﻿namespace grungus_api.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+        public class User
+        {
+            [Required]
+            [MaxLength(15)]
+            [MinLength(3)]
+            public string Name { get; set; }
+
+            [Required]
+            [MaxLength(255)]
+            [MinLength(6)]
+            public string Password { get; set; }
+
+            public string Token { get; set; }
+
+            public User(string name, string password)
+            {
+                Name = name;
+                Password = password;
+            }
+
+            public User(string name, string password, string token)
+            {
+                Name = name;
+                Password = password;
+                Token = token;
+            }
+        }
+    
+}
