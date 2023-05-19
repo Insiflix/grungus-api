8696e438eab97186e2ec1dab7ac8e5a608170372
65Juko <thejulianjuko@gmail.com>
2023-05-19 14:56:51 +0200

    added messages

--- src/routes/messages.ts
+++ src/routes/messages.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Request, Response, Router } from 'express';
+import { body } from 'express-validator';
+
+import prisma from '../tools/prismaConnection';
 
 const { requireAuth, validate } = require('../tools/middlewares');
 
@@ -8,11 +11,19 @@ export {};
 const messages: Router = Router();
 
 messages.get(
-  '/',
+  '/history',
   requireAuth,
+  body('skip').isNumeric(),
+  body('channelId').isString().notEmpty(),
   validate,
   async (req: Request, res: Response) => {
-    res.status(404).json({ msg: 'to be added' });
+    const messages = await prisma.message.findMany({
+      where: { channelId: req.body.channelId },
+      orderBy: { createdAt: 'desc' },
+      skip: 50 * req.body.skip,
+      take: 50
+    });
+    res.json(messages);
   }
 );
 
